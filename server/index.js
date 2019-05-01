2ddf40db1652e65eec177a614083b915bc1a5e4a
bryan <yiyuan@ucsd.edu>
2019-05-01 12:46:41 -0700

    set up the Express App

---
+++ server/index.js
@@ -0,0 +1,7 @@
+// we have also seen 
+// import express from 'express'
+// the require function is for the common JS module system,
+// and the import keyword is for the ES2015 module system.
+// these two systems are different and Node only supports 
+// common JS module system.
+const express = require('express');
